1d40431fdf148768d7c87d040b0e7aacc024a5f7
Ryan Dens <ryandens735@gmail.com>
2020-05-24 17:31:36 -0400

    :sparkles: add output for s3 bucket website domain name

--- modules/static-site/outputs.tf
+++ modules/static-site/outputs.tf
@@ -2,3 +2,7 @@ output "name_servers" {
   description = "the Route 53 Zone for configuring your domain name provider"
   value       = aws_route53_zone.zone.name_servers
 }
+
+output "s3_url" {
+  value = aws_s3_bucket.www.bucket_domain_name
+}
